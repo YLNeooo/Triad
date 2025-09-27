066d3d6270f3c2fdeb213b2b9bb06a9bb4163281
thomasg13 <thomasguo13@gmail.com>
2025-09-26 23:11:12 -0400

    apikey

--- cedar-app/src/lib/firebase/client.ts
+++ cedar-app/src/lib/firebase/client.ts
@@ -5,8 +5,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCRwPizrDoZv0sgoB4xkB2Uv2CUbtigzwM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "triad-19f41.firebaseapp.com",
   projectId: "triad-19f41",
   storageBucket: "triad-19f41.firebasestorage.app",
